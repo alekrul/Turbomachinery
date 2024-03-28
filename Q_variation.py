95e2aec25f635c71819b611bac5aa38d995cead0
Alexandre Krul <62573871+alekrul@users.noreply.github.com>
2024-03-28 12:09:23 -0300

    Update Q_variation.py
    varying Beta2 (vane angle at outlet) and impact on W_dot for different Q

--- Q_variation.py
+++ Q_variation.py
@@ -3,30 +3,38 @@ from math import *
 import numpy as np
 import matplotlib.pyplot as plt
 
+#understand impact on Work done by changing Q and Beta2
 r2 = 150/1000 #m
 b2 = 6.25/1000 #m
 Q = [0.10, 0.50, 0.90, 1.30, 1.70, 2.10, 2.50, 2.90, 3.30]
 omega = 750*2*pi*(1/60) #rpm to rad/s
 
-beta2 = 40
+beta2 = [radians(60), radians(90), radians(120)] #backswept, radial and front
 
 U2 = TipSpeed(omega,r2)
-W_dot = np.zeros(len(Q))
-
+W_dot = np.zeros((len(beta2),len(Q)))
 for i in range(len(Q)):
     Vn2 = flowtoVel(Q[i],r2,b2)
     print("------------------------------")
     print("Q:",Q[i])
-    W2abs = Vn2/sin(beta2)
-    W2 = Wvetorial(W2abs,beta2)
-    V2,V2abs = Vabs(U2,W2)
-    W_dot[i] = power(U2,[0,0],[0,0],V2,Q[i]*999)
+    for j in range(len(beta2)):
+        W2abs = Vn2/sin(beta2[j])
+        W2 = Wvetorial(W2abs,beta2[j])
+        V2,V2abs = Vabs(U2,W2)
+        W_dot[j][i] = power(U2,[0,0],[0,0],V2,Q[i]*999)
 
-plt.plot(Q,W_dot)
-plt.xlabel("Q")
+print("W_dot",W_dot)
+#plt.plot(Q,W_dot)
+#plt.xlabel("Q")
+#plt.ylabel(r'$\dot{W}$')
+#plt.title("Variação de "+r'$\dot{W}$'+" a partir da mudança de Q")
+#plt.show()
+
+
+for i in range(len(beta2)):
+    plt.plot(Q, W_dot[i,:], label='β = {}'.format(degrees(beta2[i])))
+
+plt.xlabel('Q')
 plt.ylabel(r'$\dot{W}$')
-plt.title("Variação de "+r'$\dot{W}$'+" a partir da mudança de Q")
+plt.legend()
 plt.show()
-
-
-
